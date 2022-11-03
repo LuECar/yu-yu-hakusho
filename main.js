e00a9c55f155dc31e451d4bcafdba8c05ce4cf56
LuECar <LuECar@github.com>
2022-11-03 00:34:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById('yuske').onclick = Yuske
+document.getElementById('kuwabara').onclick = Kuwabara  
+document.getElementById('heie').onclick = Heie
+document.getElementById('kurama').onclick = Kurama
+
+function Yuske() {
+   document.body.style.backgroundImage = "url('https://i.imgur.com/FqRjKP9.gif')";
+   document.body.style.backgroundRepeat = "no-repeat"
+   document.body.style.backgroundSize = "cover"
+}
+
+function Kuwabara() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/oBK7g8h.gif')";
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+}
+
+function Heie() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/xes4J2L.gif')";
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+}
+
+function Kurama() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/jLODQrb.gif')";
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+ }
